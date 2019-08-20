213eaec16c3c16cc57fb72d2a5ce90552a48e2d6
Aman Rusia <rusia.aman@gmail.com>
2019-08-21 01:59:55 +0530

    Added init checkpoint

--- tf/train.py
+++ tf/train.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 
 import math
 import time
+import collections
+import re
 
 from absl import flags
 import absl.logging as _logging  # pylint: disable=unused-import
@@ -13,12 +15,12 @@ from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 from tensorflow.gfile import Exists as exists
 import model
-import data_utils_xlnet as data_utils
 import tpu_estimator
 
 import numpy as np
 from time import sleep
 
+import data_utils_xlnet as data_utils
 
 # TPU parameters
 flags.DEFINE_string("master", default=None,
@@ -139,6 +141,9 @@ flags.DEFINE_float("proj_init_std", default=0.01,
       help="Initialization std for embedding projection.")
 flags.DEFINE_float("init_range", default=0.1,
       help="Initialization std when init is uniform.")
+# Checkpoint
+flags.DEFINE_string("init_checkpoint", default=None,
+                    help="checkpoint path for initializing the model for training.")
 
 
 FLAGS = flags.FLAGS
@@ -151,6 +156,70 @@ def metric_fn(loss):
       "perplexity": tf.metrics.mean(perplexity),
       "bpc": tf.metrics.mean(bpc),
   }
+
+
+def get_assignment_map_from_checkpoint(tvars, init_checkpoint):
+  """Compute the union of the current variables and checkpoint variables."""
+  assignment_map = {}
+  initialized_variable_names = {}
+
+  name_to_variable = collections.OrderedDict()
+  for var in tvars:
+    name = var.name
+    m = re.match("^(.*):\\d+$", name)
+    if m is not None:
+      name = m.group(1)
+    name_to_variable[name] = var
+
+  init_vars = tf.train.list_variables(init_checkpoint)
+
+  assignment_map = collections.OrderedDict()
+  for x in init_vars:
+    (name, var) = (x[0], x[1])
+    # tf.logging.info('original name: %s', name)
+    if name not in name_to_variable:
+      continue
+    # assignment_map[name] = name
+    assignment_map[name] = name_to_variable[name]
+    initialized_variable_names[name] = 1
+    initialized_variable_names[name + ":0"] = 1
+
+  return (assignment_map, initialized_variable_names)
+
+def init_from_checkpoint_scaffold(global_vars=False):
+  tvars = tf.global_variables() if global_vars else tf.trainable_variables()
+  initialized_variable_names = {}
+  scaffold_fn = None
+  if FLAGS.init_checkpoint is not None:
+    if FLAGS.init_checkpoint.endswith("latest"):
+      ckpt_dir = os.path.dirname(FLAGS.init_checkpoint)
+      init_checkpoint = tf.train.latest_checkpoint(ckpt_dir)
+    else:
+      init_checkpoint = FLAGS.init_checkpoint
+
+    tf.logging.info("Initialize from the ckpt {}".format(init_checkpoint))
+
+    (assignment_map, initialized_variable_names
+    ) = get_assignment_map_from_checkpoint(tvars, init_checkpoint)
+    if FLAGS.use_tpu:
+      def tpu_scaffold():
+        tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
+        return tf.train.Scaffold()
+
+      scaffold_fn = tpu_scaffold
+    else:
+      tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
+
+    # Log customized initialization
+    tf.logging.info("**** Global Variables ****")
+    for var in tvars:
+      init_string = ""
+      if var.name in initialized_variable_names:
+        init_string = ", *INIT_FROM_CKPT*"
+      tf.logging.info("  name = %s, shape = %s%s", var.name, var.shape,
+                      init_string)
+  return scaffold_fn
+
 
 
 def get_model_fn():
@@ -236,6 +305,8 @@ def get_model_fn():
 
     total_loss = tf.reduce_mean(loss)
 
+    scaffold_fn = init_from_checkpoint_scaffold()
+
     if mode == tf.estimator.ModeKeys.EVAL:
       if FLAGS.use_tpu:
         with tf.colocate_with(total_loss):
@@ -245,7 +316,8 @@ def get_model_fn():
       eval_spec = tf.contrib.tpu.TPUEstimatorSpec(
           mode=mode,
           loss=total_loss,
-          eval_metrics=(metric_fn, [metric_loss]))
+          eval_metrics=(metric_fn, [metric_loss]),
+          scaffold_fn=scaffold_fn)
 
       eval_spec.cache = new_mems
 
